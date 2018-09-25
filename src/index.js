b7a9e4f88de684a86bca02a6924e5b8b7877ed1a
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2018-09-25 22:33:44 +0300

    Quadratic equation/feature: implement function to solve equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const array = equation.split(' ');
+  const a = array[0];
+  const b = array[3] + array[4]; 
+  const c = array[7] + array[8];
+  const dRoot = Math.sqrt(Math.pow(b,2) - (4 * a * c));
+	const x1 = Math.round(eval((- b + dRoot) / (2 * a)));
+  const x2 = Math.round(eval((- b - dRoot) / (2 * a)));
+  const result = [];
+  result.push(x1, x2);
+  result.sort(function(a, b){return a - b});
+  return result;
 }
